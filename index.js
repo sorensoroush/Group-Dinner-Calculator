8317d386c016888799e9e2d0a697fb85aad68388
sorensoroush <soren1338@gmail.com>
2023-04-26 21:56:36 -0400

    Add tax slider and output

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ document.querySelector('#tip-form').onchange = function(){
 
   var bill = Number(document.getElementById('billTotal').value);
   var tip = document.getElementById('tipInput').value;
+  var tax = document.getElementById('taxInput').value;
   document.getElementById('tipOutput').innerHTML = `${tip}%`;
+  document.getElementById('taxOutput').innerHTML = `${tax}%`;
   var tipValue = bill * (tip/100)
-  var finalBill = bill + tipValue
+  var taxValue = bill * (tax/100)
+  var finalBill = bill + taxValue + tipValue
 console.log(finalBill)
 var tipAmount = document.querySelector('#tipAmount')
+var taxAmount = document.querySelector('#taxAmount')
 var totalBillWithTip = document.querySelector('#totalBillWithTip')
 
 tipAmount.value = tipValue.toFixed(2);
+taxAmount.value = taxValue.toFixed(2);
  totalBillWithTip.value =finalBill.toFixed(2);
 
  //Show Results
