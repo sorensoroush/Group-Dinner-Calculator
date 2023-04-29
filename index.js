c55004ea4bff957b26cca895818a94e5cd390517
soren1338 <soren1338@gmail.com>
2023-04-29 17:33:53 -0400

    Add basic cost input appending

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 nameInput = document.querySelector('#name-input')
 nameSubmit = document.querySelector('#name-submit')
+costInputs = document.querySelector('#cost-inputs')
+
+const costInputTemplate = name => {
+	costInputArt = document.createElement('article')
+	costInputArt.setAttribute('class','cost-input-art')
+	costInputTest = document.createElement('h3')
+	costInputTest.textContent = name
+	costInputArt.append(costInputTest)
+	return costInputArt
+}
 
 nameSubmit.addEventListener('click', () => {
     console.log(nameInput.value)
+	costInputs.append(costInputTemplate(nameInput.value))
     nameInput.value = ''
 })
