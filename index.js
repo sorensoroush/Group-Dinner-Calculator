c701db8373b5e51d04daad638da13fa7c12c6b5f
soren1338 <soren1338@gmail.com>
2023-04-30 17:21:16 -0400

    Member and tip/tax inputs now update totals

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
 nameInput = document.querySelector('#name-input')
 nameSubmit = document.querySelector('#name-submit')
-costInputs = document.querySelector('#cost-inputs')
+costInputsDiv = document.querySelector('#cost-inputs')
 tipTaxInput = document.querySelector('#tip-tax-input')
 taxInput = document.querySelector('#tax-input')
 tipInput = document.querySelector('#tip-input')
-costOutputs = document.querySelector('#cost-outputs')
+tipDisplay = document.querySelector('#tip-display')
+totalCostDisplay = document.querySelector('#total-cost-display')
+completeCostDisplay = document.querySelector('#complete-cost-display')
+costOutputsDiv = document.querySelector('#cost-outputs')
 
 let inputsNum = 0
 let totalCost = 0
 let taxCost = 0
-let tipCost = 0
+let tipPercentage = 0
 let completeCost = 0
 
 const updateCosts = () => {
-
+	totalCost = 0
+	completeCost = 0
+	taxCost = Number(taxInput.value)
+	tipPercentage = Number(tipInput.value) / 100
+	costInputs = document.querySelectorAll('#cost-inputs input')
+	costInputs.forEach(input => {
+		totalCost += Number(input.value)
+	})
+	totalCostDisplay.value = totalCost.toFixed(2)
+	completeCost = totalCost + taxCost + tipPercentage*totalCost
+	completeCostDisplay.value = completeCost.toFixed(2)
 }
 
 const costTemplate = (name,type) => {
 	costArt = document.createElement('article')
 	costArt.setAttribute('class','cost-art')
 	costLabel = document.createElement('label')
-	costLabel.textContent = `${name}:`
+	costLabel.textContent = `${name}: `
 	costInput = document.createElement('input')
 	costInput.setAttribute('type','number')
 	if (type === 'input') {
 		costInput.setAttribute('id',`cost-input-${inputsNum}`)
 		costInput.setAttribute('placeholder','Cost')
-		costInput.addEventListener('input', updateCosts())
+		costInput.addEventListener('input', updateCosts)
 	} else if (type === 'output') {
 		costInput.setAttribute('id',`cost-output-${inputsNum}`)
 		costInput.setAttribute('disabled','')
@@ -38,14 +51,19 @@ const costTemplate = (name,type) => {
 
 const createName = () => {
 	if (nameInput.value) {
-		costInputs.append(costTemplate(nameInput.value,'input'))
-		costOutputs.append(costTemplate(nameInput.value,'output'))
+		costInputsDiv.append(costTemplate(nameInput.value,'input'))
+		costOutputsDiv.append(costTemplate(nameInput.value,'output'))
 		inputsNum++
 		nameInput.value = ''
 	}
 }
 
-nameSubmit.addEventListener('click', createName())
+nameSubmit.addEventListener('click', createName)
 nameInput.addEventListener('keydown', e => {
 	if (e.code === 'Enter') {createName()}
+})
+taxInput.addEventListener('input', updateCosts)
+tipInput.addEventListener('input', () => {
+	tipDisplay.textContent = `${tipInput.value}%`
+	updateCosts()
 })
