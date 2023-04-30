760cd263484fac0bd5aa63d6a5fb7beed7236d52
soren1338 <soren1338@gmail.com>
2023-04-30 14:58:03 -0400

    Change input type text=>number, refactor nameinput event listener

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ nameInput = document.querySelector('#name-input')
 nameSubmit = document.querySelector('#name-submit')
 costInputs = document.querySelector('#cost-inputs')
 tipTaxInput = document.querySelector('#tip-tax-input')
+taxInput = document.querySelector('#tax-input')
+tipInput = document.querySelector('#tip-input')
 costOutputs = document.querySelector('#cost-outputs')
 
 let inputsNum = 0
+let totalCost = 0
+let taxCost = 0
+let tipCost = 0
+let completeCost = 0
+
+const updateCosts = () => {
+
+}
 
 const costTemplate = (name,type) => {
 	costArt = document.createElement('article')
@@ -12,11 +22,11 @@ const costTemplate = (name,type) => {
 	costLabel = document.createElement('label')
 	costLabel.textContent = `${name}:`
 	costInput = document.createElement('input')
-	costInput.setAttribute('type','text')
+	costInput.setAttribute('type','number')
 	if (type === 'input') {
 		costInput.setAttribute('id',`cost-input-${inputsNum}`)
-		costInput.setAttribute('inputmode','numeric')
 		costInput.setAttribute('placeholder','Cost')
+		costInput.addEventListener('input', updateCosts())
 	} else if (type === 'output') {
 		costInput.setAttribute('id',`cost-output-${inputsNum}`)
 		costInput.setAttribute('disabled','')
@@ -26,12 +36,16 @@ const costTemplate = (name,type) => {
 	return costArt
 }
 
-nameSubmit.addEventListener('click', () => {
+const createName = () => {
 	if (nameInput.value) {
 		costInputs.append(costTemplate(nameInput.value,'input'))
 		costOutputs.append(costTemplate(nameInput.value,'output'))
 		inputsNum++
 		nameInput.value = ''
-		tipTaxInput.style.display = 'block'
 	}
+}
+
+nameSubmit.addEventListener('click', createName())
+nameInput.addEventListener('keydown', e => {
+	if (e.code === 'Enter') {createName()}
 })
