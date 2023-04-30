9c11c616443eb274c13fe86629bbdfaf8177e3df
soren1338 <soren1338@gmail.com>
2023-04-30 10:13:24 -0400

    Add all i/o fields, no i/o logic

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 nameInput = document.querySelector('#name-input')
 nameSubmit = document.querySelector('#name-submit')
 costInputs = document.querySelector('#cost-inputs')
+tipTaxInput = document.querySelector('#tip-tax-input')
+costOutputs = document.querySelector('#cost-outputs')
 
-const costInputTemplate = name => {
-	costInputArt = document.createElement('article')
-	costInputArt.setAttribute('class','cost-input-art')
-	costInputTest = document.createElement('h3')
-	costInputTest.textContent = name
-	costInputArt.append(costInputTest)
-	return costInputArt
+let inputsNum = 0
+
+const costTemplate = (name,type) => {
+	costArt = document.createElement('article')
+	costArt.setAttribute('class','cost-art')
+	costLabel = document.createElement('label')
+	costLabel.textContent = `${name}:`
+	costInput = document.createElement('input')
+	costInput.setAttribute('type','text')
+	if (type === 'input') {
+		costInput.setAttribute('id',`cost-input-${inputsNum}`)
+		costInput.setAttribute('inputmode','numeric')
+		costInput.setAttribute('placeholder','Cost')
+	} else if (type === 'output') {
+		costInput.setAttribute('id',`cost-output-${inputsNum}`)
+		costInput.setAttribute('disabled','')
+	}
+	costLabel.append(costInput)
+	costArt.append(costLabel)
+	return costArt
 }
 
 nameSubmit.addEventListener('click', () => {
-    console.log(nameInput.value)
-	costInputs.append(costInputTemplate(nameInput.value))
-    nameInput.value = ''
+	if (nameInput.value) {
+		costInputs.append(costTemplate(nameInput.value,'input'))
+		costOutputs.append(costTemplate(nameInput.value,'output'))
+		inputsNum++
+		nameInput.value = ''
+		tipTaxInput.style.display = 'block'
+	}
 })
